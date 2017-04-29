5bac45ae73ed8d3cbbfc633968eb8766cccebf25
XiaoYunxuan <1085966850@qq.com>
2017-04-29 23:25:44 +0800

    operator< & debug

--- src/Date.hpp
+++ src/Date.hpp
@@ -26,18 +26,29 @@ class Date{
 				year = year * 10 + str[i] - '0';
 				month = (str[5] - '0')*10 + str[6] - '0';
 				day = (str[8] - '0')*10 + str[9] - '0';
+				cout<<year<<" "<<month<<" "<<day<<endl;
 		}
 		string GetDate()const{
-			string str;
+			char str[11];
 			int y = year, m = month , d = day;
 			for(int i = 3;i >= 0;i--,y/=10)
-				str[i] = y % 10;
+				str[i] = y % 10 + '0';
 			for(int i = 6;i >= 5;i--,m/=10)
-				str[i] = m % 10;
+				str[i] = m % 10 + '0';
 			for(int i = 9;i >= 8;i--,d/=10)
-				str[i] = d % 10;	
+				str[i] = d % 10 + '0';	
 				str[4] = str[7]  = '-';
-			return str;	
+				str[10] = '\0';
+			string ans = str;
+			return ans;	
+		}
+		bool operator<(const Date &date){          //小于 即早于 
+			if(year > date.year)return false;
+			else if (year < date.year)return true;
+			if(month > date.month)return false;
+			else if (month < date.month)return true;
+			if(day >= date.day)return false;
+			else if (day < date.day)return true;
 		}
 		friend binofstream& operator<<(binofstream &fout, const Date& date){   //友元函数无法加const？... 
 			string str = date.GetDate();
